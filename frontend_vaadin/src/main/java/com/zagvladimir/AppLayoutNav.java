2b310179c2c3366d3f1ea6d37b943e58f4f6f68f
Vladimir Zagorny <zagvladimir88@gmail.com>
2023-02-08 16:13:04 +0300

    save point

---
+++ frontend_vaadin/src/main/java/com/zagvladimir/AppLayoutNav.java
@@ -0,0 +1,42 @@
+package com.zagvladimir;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
+
+@Route("")
+public class AppLayoutNav extends AppLayout {
+
+    public AppLayoutNav() {
+        H1 title = new H1("MyApp");
+        title.getStyle().set("font-size", "var(--lumo-font-size-l)")
+                .set("left", "var(--lumo-space-l)").set("margin", "0")
+                .set("position", "absolute");
+
+        Tabs tabs = getTabs();
+
+        addToNavbar(title, tabs);
+    }
+
+    private Tabs getTabs() {
+        Tabs tabs = new Tabs();
+        tabs.getStyle().set("margin", "auto");
+        tabs.add(createTab("Dashboard"), createTab("Orders"),
+                createTab("Customers"), createTab("Products"));
+        return tabs;
+    }
+
+    private Tab createTab(String viewName) {
+        RouterLink link = new RouterLink();
+        link.add(viewName);
+        // Demo has no routes
+        // link.setRoute(viewClass.java);
+        link.setTabIndex(-1);
+
+        return new Tab(link);
+    }
+
+}
---
+++ frontend_vaadin/src/main/java/com/zagvladimir/view/Test.java
@@ -0,0 +1,58 @@
+package com.zagvladimir.view;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
+
+@Route("/test")
+public class Test extends AppLayout {
+
+    public Test() {
+        DrawerToggle toggle = new DrawerToggle();
+
+        H1 title = new H1("MyApp");
+        title.getStyle().set("font-size", "var(--lumo-font-size-l)")
+                .set("margin", "0");
+
+        Tabs tabs = getTabs();
+
+        addToDrawer(tabs);
+        addToNavbar(toggle, title);
+    }
+
+    private Tabs getTabs() {
+        Tabs tabs = new Tabs();
+        tabs.add(createTab(VaadinIcon.DASHBOARD, "Dashboard"),
+                createTab(VaadinIcon.CART, "Orders"),
+                createTab(VaadinIcon.USER_HEART, "Customers"),
+                createTab(VaadinIcon.PACKAGE, "Products"),
+                createTab(VaadinIcon.RECORDS, "Documents"),
+                createTab(VaadinIcon.LIST, "Tasks"),
+                createTab(VaadinIcon.CHART, "Analytics"));
+        tabs.setOrientation(Tabs.Orientation.VERTICAL);
+        return tabs;
+    }
+
+    private Tab createTab(VaadinIcon viewIcon, String viewName) {
+        Icon icon = viewIcon.create();
+        icon.getStyle().set("box-sizing", "border-box")
+                .set("margin-inline-end", "var(--lumo-space-m)")
+                .set("margin-inline-start", "var(--lumo-space-xs)")
+                .set("padding", "var(--lumo-space-xs)");
+
+        RouterLink link = new RouterLink();
+        link.add(icon, new Span(viewName));
+        // Demo has no routes
+        // link.setRoute(viewClass.java);
+        link.setTabIndex(-1);
+
+        return new Tab(link);
+    }
+
+}
